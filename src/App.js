6bb6cb52ce71cdf1e017c72c6e6c2b77fe1f6f63
praneetha002 <praneetharatna.21bce9964@vitapstudent.ac.in>
2023-11-10 16:28:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch('https://dummyjson.com/users')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data && Array.isArray(data.users)) {
+          this.setState({ users: data.users, isLoading: false });
+        } else {
+          this.setState({ error: new Error('Invalid data format'), isLoading: false });
+        }
+      })
+      .catch((error) => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const { users, isLoading, error } = this.state;
+
+    return (
+      <div>
+        <h1>Dummy Data</h1>
+        {error && <div>Error: {error.message}</div>}
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Sno</th>
+                <th>Profile Pic</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>University</th>
+                <th>Username</th>
+                <th>IP</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <img src={user.image} alt={`${user.firstName} ${user.lastName}`} width="50" />
+                  </td>
+                  <td>{user.firstName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.university}</td>
+                  <td>{user.username}</td>
+                  <td>{user.ip}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
